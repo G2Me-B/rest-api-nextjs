00c74211f349419d05b45ab51023cf69bb34fc2a
G2Me-B <brutehackenstein@gmail.com>
2025-12-22 22:26:02 +0000

    third commit

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -72,3 +72,43 @@ export const PATCH = async (request: Request) => {
         return new NextResponse("Error in updating user: " + (error instanceof Error ? error.message : String(error)), { status: 500 });
     }
 }
+
+
+export const DELETE = async (request: Request) => {
+    try {
+        const {searchParams} = new URL(request.url);
+        const userId = searchParams.get('userId');
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "userId " }),
+                { status: 400 }
+            );
+        }
+        if(!userId){
+            return new NextResponse(
+                JSON.stringify({ message: "userId not found" }),
+                { status: 400 }
+            );
+        }
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid userId" }),
+                { status: 400 }
+            );
+        }
+        
+        await connect();
+
+        const deletedUser = await User.findOneAndDelete(new Types.ObjectId(userId));
+        if(!deletedUser){
+            return new NextResponse(
+                JSON.stringify({ message: "User not found in the database" }),
+                { status: 400 }
+            )
+        }
+        return new NextResponse(JSON.stringify({ message: "User deleted successfully", user: deletedUser }), { status: 200 });
+    } catch (error: unknown) {
+        const msg = error instanceof Error ? error.message : String(error);
+        return new NextResponse("Error in deleting user: " + msg, { status: 500 });
+    }
+}
